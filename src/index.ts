440f07e5a2c135d0ba713ae2a4c9ade51b948729
tanmay <kuamrchaurasiatanmay@gmail.com>
2024-10-31 13:11:02 +0530

    server working

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,44 @@
-import { Hono } from 'hono'
+import { createRoute } from '@hono/zod-openapi'
+import { OpenAPIHono } from '@hono/zod-openapi'
+import { UserSchema } from './output'
+import { ParamsSchema } from './input'
 
-const app = new Hono()
+const route = createRoute({
+  method: 'get',
+  path: '/users/{id}',
+  request: {
+    params: ParamsSchema,
+  },
+  responses: {
+    200: {
+      content: {
+        'application/json': {
+          schema: UserSchema,
+        },
+      },
+      description: 'Retrieve the user',
+    },
+  },
+})
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+const app = new OpenAPIHono()
+
+app.openapi(route, (c) => {
+  const { id } = c.req.valid('param')
+  return c.json({
+    id,
+    age: 20,
+    name: 'tanmay kumar chaurasia',
+  })
+})
+
+// The OpenAPI documentation will be available at /doc
+app.doc('/doc', {
+  openapi: '3.0.0',
+  info: {
+    version: '1.0.0',
+    title: 'My API',
+  },
 })
 
 export default app
